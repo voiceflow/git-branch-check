b258b61eb8fdb1b920ca9055de49daa3650d2e83
Matheus Poleza <matheus.poleza@voiceflow.com>
2022-07-05 10:11:49 -0300

    fix: adds nlu ticket pattern support (VF-000)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 const branchName = require('current-git-branch');
 
-const branchRegex = /^\w+\/(\S+\/)?(VF|CT)-\d+$/;
+const branchRegex = /^\w+\/(\S+\/)?(VF|CT|NLU)-\d+$/;
 const EXCEPTIONS = [/^production$/, /^trying$/, /^staging$/, /^master$/, /^break-glass.*$/];
 
 try {
